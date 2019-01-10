17b44b9a2f96cec5759f53e9ff2987e93e168925
hu_pf <hu_pf@suixingpay.com>
2019-01-10 14:36:49 +0800

    1

---
+++ src/main/java/com/example/springpractice/springpractice/SpringpracticeApplication.java
@@ -0,0 +1,14 @@
+package com.example.springpractice.springpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringpracticeApplication.class, args);
+	}
+
+}
+
